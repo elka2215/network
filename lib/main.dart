fc328fc65684fc2d1dbe5ce51b9098cf21b39c84
Yeldar Orazgaliev <Orazgaliev99@mail.ru>
2022-04-11 11:15:15 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:network/models/model.dart';
+import 'package:network/network_repo.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final DioClient _client = DioClient();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: FutureBuilder<User?>(
+          future: _client.getUser(id: '1'),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              User? userInfo = snapshot.data;
+              if (userInfo != null) {
+                Data userData = userInfo.data;
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.network(userData.avatar),
+                    SizedBox(height: 8.0),
+                    Text(
+                      '${userInfo.data.firstName} ${userInfo.data.lastName}',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                    Text(
+                      userData.email,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                );
+              }
+            }
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
